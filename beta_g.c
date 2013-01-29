e5e03f1ecef5ab5e19c780ba7349816c9e13e908
Jonatas Eduardo da Silva Cesar <jonataseduardo@gmail.com>
2013-01-29 16:56:34 -0200

    Adicionando um codigo em c

---
+++ beta_g.c
@@ -0,0 +1,43 @@
+/*#include <stdlib.h>*/
+#include <math.h>
+
+void dist_beta
+(  
+double ** X
+, double ** Y
+, int genes
+, int nsamplex
+, int nsampley
+, double * beta_g
+)
+{
+    int g; 
+    for ( g = 0; g < genes; g++) {
+        double x = 0, y = 0, z = 0;
+        int i, j;
+
+        for ( i = 0; i < nsamplex; i++) {
+            for ( j = i + 1; j < nsamplex; j++) {
+                x += (X[g][i] - X[g][j])*(X[g][i] - X[g][j]);
+            }
+        }
+        x = x /( nsamplex * ( nsamplex - 1. ) );
+
+        for ( i = 0; i < nsampley; i++) {
+            for ( j = i + 1; j < nsampley; j++) {
+                y += (Y[g][i] - Y[g][j])*(Y[g][i] - Y[g][j]);
+            }
+        }
+        y = y /( nsampley * ( nsampley - 1. ) );
+
+        for ( i = 0; i < nsampley; i++) {
+            for ( j = 0; j < nsamplex; j++) {
+                z += (Y[g][i] - X[g][j])*(Y[g][i] - X[g][j]);
+            }
+        }
+        z = z / ( nsamplex * nsampley );
+
+        beta_g[g] = x + y - z;
+    }
+
+}
